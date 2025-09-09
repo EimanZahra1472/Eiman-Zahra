00db9d57d44bf9a6556b5821553aed43a7adb4ad
EimanZahra1472 <zahraeiman20@gmail.com>
2025-09-09 16:10:59 +0500

    Add utilities module with add(a,b)

---
+++ utilities.cpp
@@ -0,0 +1,3 @@
+// utilities.cpp
+#include "utilities.h"
+int add(int a, int b) { return a + b; }
---
+++ utilities.h
@@ -0,0 +1,7 @@
+// utilities.h
+#ifndef UTILITIES_H
+#define UTILITIES_H
+
+int add(int a, int b);  // placeholder example
+
+#endif
